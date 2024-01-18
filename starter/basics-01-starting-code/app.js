78ef54fde32d1520a03e5f598dedef15a47d0987
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-01-18 18:29:58 +0200

    Working with Data inside of a Vue App

--- starter/basics-01-starting-code/app.js
+++ starter/basics-01-starting-code/app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finish the course and learn Vue!",
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "Master Vue and build amazing Apps!",
       vueLink: "https://vuejs.org/",
     };
   },
   methods: {
     outputGoal() {
       const randomNumber = Math.random();
-      return randomNumber < 0.5 ? "Learn Vue!" : "Master Vue!";
+      return randomNumber < 0.5 ? this.courseGoalA : this.courseGoalB;
     },
   },
 });
