46d54d141731793d54fd403f68eb2494ec7c3944
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-02-21 18:55:01 +0200

    Exploring Event Modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
+    setName(event, lastName) {
+      this.name = e.target.value + " " + lastName;
+    },
     add(num) {
       this.counter = this.counter + 5;
     },
