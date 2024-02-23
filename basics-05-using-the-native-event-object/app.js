63a576aa022420ad575ce60144d5729c34d3738b
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-02-23 18:51:17 +0200

    Working with Watchers

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -3,15 +3,41 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      // fullname: "",
+      lastname: "",
     };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastname;
+    //   }
+    // },
+    // lastname(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
   },
   computed: {
     fullName() {
       console.log("Running again...");
-      if (this.name === "") {
+      if (this.name === "" || this.lastname === "") {
         return "";
       }
-      return this.name + " " + "Voronevska";
+      return this.name + " " + this.lastname;
     },
   },
   methods: {
