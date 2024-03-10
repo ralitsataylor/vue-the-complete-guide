543bdccd2b3bdd0605ba3966cddfd21158b9f2a0
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-03-11 00:03:19 +0200

    Assignment 4: Time to Practice: Dynmic Styling

---
+++ basics-assignment-4-problem/app.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      inputClass: "",
+      paragraphIsVisible: true,
+      backgroundColor: "",
+    };
+  },
+  computed: {
+    paragraphClasses() {
+      return {
+        user1: this.inputClass === "user1",
+        user2: this.inputClass === "user2",
+        visible: this.paragraphIsVisible,
+        hidden: !this.paragraphIsVisible,
+      };
+    },
+  },
+  methods: {
+    toggleParagraphVisibility() {
+      this.paragraphIsVisible = !this.paragraphIsVisible;
+    },
+  },
+});
+app.mount("#assignment");
