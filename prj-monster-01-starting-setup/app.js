953ff19398f51e0d77e6cced0fe0457e9b98caf4
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-03-12 17:21:47 +0200

    Adding a "Special Attack"

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
   },
   computed: {
@@ -16,9 +17,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return { width: this.playerHealth + "%" };
     },
+    mayUseSpecialAttack() {
+      return currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       // Formula for calculating a Random Number between 5 and 12
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
@@ -28,6 +33,12 @@ const app = Vue.createApp({
       // Formula for calculating a Random Number between 8 and 15
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
