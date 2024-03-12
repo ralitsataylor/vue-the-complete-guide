149c05041b7e7c1bfb530726de4c4e10eb5aa1f4
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-03-12 17:30:00 +0200

    Adding a "Heal" Functionality

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -18,19 +18,17 @@ const app = Vue.createApp({
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
-      return currentRound % 3 !== 0;
+      return this.currentRound % 3 !== 0;
     },
   },
   methods: {
     attackMonster() {
       this.currentRound++;
-      // Formula for calculating a Random Number between 5 and 12
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
     },
     attackPlayer() {
-      // Formula for calculating a Random Number between 8 and 15
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
     },
@@ -38,6 +36,16 @@ const app = Vue.createApp({
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
       this.attackPlayer();
     },
   },
