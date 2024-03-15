c6ac48dd8498663fb0e4808e17cb7e36dded2962
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-03-15 17:43:45 +0200

    Introducing Components

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,14 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: " 01234 5678 991",
-          email: "manuel@localhost.com",
-        },
         {
           id: "julie",
           name: "Julie Jones",
@@ -18,10 +11,37 @@ const app = Vue.createApp({
       ],
     };
   },
+});
+
+app.component("friend-contact", {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "Manuel Lorenz",
+        phone: " 01234 5678 991",
+        email: "manuel@localhost.com",
+      },
+    };
+  },
   methods: {
     toggleDetails() {
       this.detailsAreVisible = !this.detailsAreVisible;
     },
   },
 });
+
 app.mount("#app");
