d8de00fad1d1c8f65370e61a6a747806fa07832e
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-07-25 15:33:43 +0300

    Introducing "Props" (Parent => Child Communication)

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -2,8 +2,13 @@
   <section>
     <header><h1>My Friends</h1></header>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        v-for="friend in friends"
+        :key="friend"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :email-address="friend.email"
+      ></friend-contact>
     </ul>
   </section>
 </template>
