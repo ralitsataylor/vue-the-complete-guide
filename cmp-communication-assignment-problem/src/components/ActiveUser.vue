b76797d8cb49d6999386e3076f35ce319a64e5d3
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2024-08-05 00:49:07 +0300

    Assignment 6: Time to Practice: Props & Custom Events

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h3>{{ username }}</h3>
+    <h2>{{ userage }} Years</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userage: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
