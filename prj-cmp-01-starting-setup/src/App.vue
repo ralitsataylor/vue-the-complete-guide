3fc65b887adf47320466b415e6939d07f8b10eed
ralitsavoronevska <ralicavoronevska@gmail.com>
2024-09-06 23:47:53 +0300

    Setup & First Steps

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource">
+      <p>{{ resource.id }}</p>
+      <p>{{ resource.title }}</p>
+      <p>{{ resource.descriotion }}</p>
+      <p>{{ resource.link }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
