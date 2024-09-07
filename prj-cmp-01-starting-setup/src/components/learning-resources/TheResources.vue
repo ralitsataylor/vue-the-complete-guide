d2c8485d018ccee127e53c87322e6b23744ffbf4
ralitsavoronevska <ralicavoronevska@gmail.com>
2024-09-07 22:56:49 +0300

    Dynamic Components & Attribute Fallthrough

---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,30 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="setSelectedTab(tab)"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResources from './AddResources.vue';
+
+export default {
+  components: { StoredResources, AddResources },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+};
+</script>
