a634708af615bd5ebf767160fdfeafa991c6e586
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-01-11 18:21:41 +0200

    Routing Setup

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.mount('#app');
