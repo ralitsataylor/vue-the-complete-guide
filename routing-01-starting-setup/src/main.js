6b5645f217dd73c399aded13cc8bb182f195d164
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-01-12 16:11:32 +0200

    Redirecting & "Catch All" Routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -5,13 +5,16 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' }, // our-domamin.com => TeamsList
     { path: '/teams', component: TeamsList }, // our-domamin.com/teams => TeamsList
     { path: '/users', component: UsersList }, // our-domamin.com/users => UsersList
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    { path: '/teams/:teamId', component: TeamMembers, props: true }, // our-domamin.com/:teamId => TeamMembers
+    { path: '/:notFound(.*)', component: NotFound }, // our-domamin.com/something => NotFound
   ],
   linkActiveClass: 'active',
 });
