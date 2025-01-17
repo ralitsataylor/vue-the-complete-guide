3d9994c6b29585484e2faf094c0fe30dc6449d75
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-01-17 18:12:57 +0200

    Example: Adding More State

--- vuex-01-starting-setup/src/components/UserAuth.vue
+++ vuex-01-starting-setup/src/components/UserAuth.vue
@@ -1,22 +1,25 @@
 <template>
-  <button @click="login" v-if="!isAuth">Login</button>
+  <button @click="login" v-if="!userIsAuthenticated">Login</button>
   <button @click="logout" v-else>Logout</button>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex/dist/vuex.cjs.js';
 export default {
   methods: {
-    login() {
-      this.$store.dispatch('login');
-    },
-    logout() {
-      this.$store.dispatch('logout');
-    },
+    ...mapActions(['login', 'logout']),
+    // login() {
+    //   this.$store.dispatch('login');
+    // },
+    // logout() {
+    //   this.$store.dispatch('logout');
+    // },
   },
   computed: {
-    isAuth() {
-      return this.$store.getters.userIsAuthenticated;
-    },
+    ...mapGetters(['userIsAuthenticated']),
+    // isAuth() {
+    //   return this.$store.getters.userIsAuthenticated;
+    // },
   },
 };
 </script>
