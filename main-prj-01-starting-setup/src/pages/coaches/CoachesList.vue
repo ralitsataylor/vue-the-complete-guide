a6a1b5e67d70afb035d1969043fac4cf5088826b
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-02-12 20:05:50 +0200

    Wiring Up Pages

--- main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -1,5 +1,9 @@
 <template>
   <section>FILTER</section>
+  <div class="controls">
+    <button>Refresh</button>
+    <router-link to="/register">Register as Coach</router-link>
+  </div>
   <section>LIST OF COACHES</section>
 </template>
 
