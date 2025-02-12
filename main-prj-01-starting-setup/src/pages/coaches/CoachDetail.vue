9e036575f638b54ec83239ee60877379bfad7b10
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-02-12 21:52:37 +0200

    Working on the Coaches List and List Items

--- main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
+++ main-prj-01-starting-setup/src/pages/coaches/CoachDetail.vue
@@ -1,5 +1,15 @@
 <template>
   DETAILS FOR COACH
   <router-view></router-view>
-  <router-link to="/coaches/c1/contact">Contact</router-link>
+  <router-link :to="coachContactLink">Contact</router-link>
 </template>
+
+<script>
+export default {
+  computed: {
+    coachContactLink() {
+      return `${this.$route.path}/${this.id}/contact`; // /coaches/c1/contact
+    },
+  },
+};
+</script>
