bbc2326e6224e90821db49c6b663bdd51f5ccb49
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-02-18 00:29:32 +0200

    Caching Http Response Data

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -28,7 +28,11 @@ export default {
       id: userId,
     });
   },
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       'https://main-project-vue-course-cc733-default-rtdb.firebaseio.com/coaches.json'
     );
@@ -54,6 +58,7 @@ export default {
       coaches.push(coach);
 
       context.commit('setCoaches', coaches);
+      context.commit('setFetchTimestamp');
     }
   },
 };
