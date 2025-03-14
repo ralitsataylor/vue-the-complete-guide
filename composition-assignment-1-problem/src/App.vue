4c79d53315c19748bab8b8a122843c2c8afed473
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2025-03-14 22:31:07 +0200

    Task 4: Also solve the assignment with the Options API

--- composition-assignment-1-problem/src/App.vue
+++ composition-assignment-1-problem/src/App.vue
@@ -2,7 +2,7 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3 :class="{ hidden: !courseGoal.isVisible }">{{ courseGoal.heading }}</h3>
+  <h3 :class="{ hidden: !isVisible }">{{ heading }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
   <button @click="toggleVisibility">Toggle Goal</button>
   <!-- Task 3: Manage data in three ways -->
@@ -12,7 +12,7 @@
   <!-- Task 4: Also solve the assignment with the Options API -->
 </template>
 
-<script setup>
+<!-- <script setup>
 import { reactive } from 'vue';
 
 const courseGoal = reactive({
@@ -23,6 +23,22 @@ const courseGoal = reactive({
 function toggleVisibility() {
   courseGoal.isVisible = !courseGoal.isVisible;
 }
+</script> -->
+
+<script>
+export default {
+  data() {
+    return {
+      heading: 'Mastering Vue.js 3 with the Composition API and TypeScript!',
+      isVisible: true,
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+};
 </script>
 
 <style>
