14baf649686a40cdb29e151892f042e8b630026b
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2025-03-17 13:36:57 +0200

    Lifecycle Hooks in the Composition API

--- composition-01-starting-setup/src/components/UserData.vue
+++ composition-01-starting-setup/src/components/UserData.vue
@@ -4,7 +4,17 @@
 </template>
 
 <script setup>
-import { defineProps, computed, inject } from 'vue';
+import {
+  defineProps,
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 
 const props = defineProps({
   firstName: String,
@@ -12,6 +22,30 @@ const props = defineProps({
 });
 
 const userAge = inject('userAge');
+
+onBeforeMount(() => {
+  console.log('onBeforeMount');
+});
+
+onMounted(() => {
+  console.log('onMounted');
+});
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate');
+});
+
+onUpdated(() => {
+  console.log('onUpdated');
+});
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount');
+});
+
+onUnmounted(() => {
+  console.log('onUnmounted');
+});
 
 const userName = computed(() => props.firstName + ' ' + props.lastName);
 </script>
