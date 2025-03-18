5af019d804287d8c1bd1c921c60ffa76530eb4ab
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2025-03-18 11:06:16 +0200

    Migrating a First Component

--- composition-13-demo-starting-project/src/App.vue
+++ composition-13-demo-starting-project/src/App.vue
@@ -5,29 +5,20 @@
   </main>
 </template>
 
-<script>
+<script setup>
 import USER_DATA from './dummy-data.js';
 
 import UserList from './components/users/UserList.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
 
-export default {
-  components: {
-    UserList,
-    ProjectsList,
-  },
-  data() {
-    return {
-      selectedUser: null,
-      activeUsers: USER_DATA,
-    };
-  },
-  methods: {
-    selectUser(uid) {
-      this.selectedUser = this.activeUsers.find((usr) => usr.id === uid);
-    },
-  },
-};
+import { ref } from 'vue';
+
+const selectedUser = ref(null);
+const activeUsers = USER_DATA;
+
+function selectUser(uid) {
+  selectedUser.value = activeUsers.find((usr) => usr.id === uid);
+}
 </script>
 
 <style>
