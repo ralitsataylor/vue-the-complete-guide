ec73188bea6fbcb7325a3f0cebe0e7e02e0879ff
ralitsa.voronevska <ralitsa.voronevska@delasport.com>
2025-03-19 12:58:12 +0200

    Migrating a Big Component

--- composition-13-demo-starting-project/src/components/users/UserList.vue
+++ composition-13-demo-starting-project/src/components/users/UserList.vue
@@ -1,10 +1,17 @@
 <template>
   <base-container>
     <h2>Active Users</h2>
-    <base-search @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <base-search
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
     <div>
-      <button @click="sort('asc')" :class="{selected: sorting === 'asc'}">Sort Ascending</button>
-      <button @click="sort('desc')" :class="{selected: sorting === 'desc'}">Sort Descending</button>
+      <button @click="sort('asc')" :class="{ selected: sorting === 'asc' }">
+        Sort Ascending
+      </button>
+      <button @click="sort('desc')" :class="{ selected: sorting === 'desc' }">
+        Sort Descending
+      </button>
     </div>
     <ul>
       <user-item
@@ -18,7 +25,65 @@
   </base-container>
 </template>
 
-<script>
+<script setup>
+import UserItem from './UserItem.vue';
+import { defineProps, ref, computed, watch, defineEmits } from 'vue';
+
+const props = defineProps(['users']);
+defineEmits(['list-projects']);
+
+const enteredSearchTerm = ref('');
+const activeSearchTerm = ref('');
+
+const availableUsers = computed(() => {
+  let users = [];
+  if (activeSearchTerm.value) {
+    users = props.users.filter((usr) =>
+      usr.fullName.includes(activeSearchTerm.value)
+    );
+  } else if (props.users) {
+    users = props.users;
+  }
+  return users;
+});
+
+function updateSearch(val) {
+  enteredSearchTerm.value = val;
+}
+
+const sorting = ref(null);
+
+const displayedUsers = computed(() => {
+  if (!sorting.value) {
+    return availableUsers.value;
+  }
+  return availableUsers.value.slice().sort((u1, u2) => {
+    if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+      return 1;
+    } else if (sorting.value === 'asc') {
+      return -1;
+    } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+});
+
+function sort(mode) {
+  sorting.value = mode;
+}
+
+watch(enteredSearchTerm, function (newValue) {
+  setTimeout(() => {
+    if (newValue === enteredSearchTerm.value) {
+      activeSearchTerm.value = newValue;
+    }
+  }, 300);
+});
+</script>
+
+<!-- <script>
 import UserItem from './UserItem.vue';
 
 export default {
@@ -80,7 +145,7 @@ export default {
     }
   },
 };
-</script>
+</script> -->
 
 <style scoped>
 ul {
