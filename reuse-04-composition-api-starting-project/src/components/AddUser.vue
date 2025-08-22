7139568fbf32446bf0aa886fe9bb3f57f030077e
ralitsavoronevska <ralicavoronevska@gmail.com>
2025-08-22 16:49:31 +0300

    317. Custom Hooks / Composables & The Composition API

---
+++ reuse-04-composition-api-starting-project/src/components/AddUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script setup>
+import UserAlert from './UserAlert.vue';
+import useAlert from '../composables/alert.js';
+
+const [alertIsVisible, showAlert, hideAlert] = useAlert();
+</script>
